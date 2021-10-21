c1017dda77a9fd5517d5a4aed54e148c835db56d
Andrii Hanzha <ganzha_as@outlook.com>
2021-10-21 16:03:09 +0300

    Jenkins and terraform using

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,11 +1,45 @@
 pipeline {
     agent any
-
+    tools {
+        terraform 'myterraform'
+    }
     stages {
-        stage('Build') {
+        stage('Create task-definition') {
             steps {
-                echo 'Building..'
-                sh './mvnw package'
+                sh '''
+                echo [
+  {
+    "logConfiguration": {
+      "logDriver": "awslogs",
+      "options": {
+        "awslogs-group": "/ecs/application1",
+        "awslogs-region": "eu-central-1",
+        "awslogs-stream-prefix": "ecs"
+      }
+    },
+    "portMappings": [
+      {
+        "hostPort": 8080,
+        "protocol": "tcp",
+        "containerPort": 8080
+      }
+    ],
+    "cpu": 0,
+    "environment": [
+      {"name" : "MYSQL_URL", "value" : "jdbc:mysql://tf-petclininc-db.ctkrneelwtgi.eu-central-1.rds.amazonaws.com:3306/petclinic?allowPublicKeyRetrieval=true&useSSL=false"},
+      {"name" : "MYSQL_USER", "value" : "admin"},
+      {"name" : "MYSQL_PASS", "value" : "password"}
+    ],
+    "mountPoints": [],
+    "memoryReservation": 500,
+    "volumesFrom": [],
+    "image": "273714934666.dkr.ecr.eu-central-1.amazonaws.com/petclinic_repository:a1281084ead6ab580e796ed15e8e19c90da295d0",
+    "name": "tf-petclinic-task3"
+  }
+]' >>petclinic-task.json'
+                cat petclinic-task.json
+                '''
+
             }
         }
     }
---
+++ main.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+    region = "eu-central-1"
+}
+
+resource "aws_ecs_service" "tf-petclinic-service" {
+    name = "tf-petclinic-service"
+    cluster = "tf-petclinic-cluster"
+    task_definition = "${aws_ecs_task_definition.tf-petclinic-task2.arn}"
+    desired_count = 1
+    launch_type = "FARGATE"
+    network_configuration {
+        subnets = ["subnet-0d877999e486e3632", "subnet-0ea75832f42ff97ef"]
+        security_groups = ["sg-0af86a9fd113c4699"]
+        assign_public_ip = true
+    }
+}
+
+resource "aws_ecs_task_definition" "tf-petclinic-task2" {
+    family = "tf-petclinic-task3"
+    container_definitions = "${file("petclinic-task.json")}"
+    requires_compatibilities = ["FARGATE"]
+    cpu = 256
+    memory = 512
+    network_mode = "awsvpc"
+    execution_role_arn = aws_iam_role.ecsTaskExecutionRole.arn
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+    name = "ECSTaskExecutionRolePolicy-Demo"
+    assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+    #assume_role_policy = "${file("assume_role_policy.json")}"
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+    role = aws_iam_role.ecsTaskExecutionRole.name
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+    statement {
+        actions = ["sts:AssumeRole"]
+        
+        principals {
+            type        = "Service"
+            identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
