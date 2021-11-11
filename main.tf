9af0820ffc36c9097395eaae77e86095878961e7
Andrii Hanzha <ganzha_as@outlook.com>
2021-11-11 15:56:00 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,14 +40,14 @@ resource "aws_ecs_service" "tf-petclinic-service" {
     }
 }
 
-resource"aws_cloudwatch_log_group""testapp_log_group" {
+resource "aws_cloudwatch_log_group" "testapp_log_group" {
     name = "/ecs/testapp"
     retention_in_days = 30
     tags = {
         Name = "cw-log-group"
         }
     }
-resource"aws_cloudwatch_log_stream""myapp_log_stream" {
+resource "aws_cloudwatch_log_stream" "myapp_log_stream" {
     name = "test-log-stream"
     log_group_name = aws_cloudwatch_log_group.testapp_log_group.name
 }
